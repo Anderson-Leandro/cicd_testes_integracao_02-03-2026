2fc42a5cb1afd839ddf5d6a287dc80843350c4d6
Anderson-Leandro <106823753+Anderson-Leandro@users.noreply.github.com>
2026-03-03 01:00:09 +0000

    Feature: Add new tests

--- testes/math.test.js
+++ testes/math.test.js
@@ -1,11 +1,49 @@
 const math = require('../math');
 
 
-
 test('soma 2 + 3 igual a 5', () => {
     expect(math.add(2, 3)).toBe(5);
 });
 
 test('soma 5 + 3 é igual a 8', () => {
     expect(math.add(5, 3)).toBe(8);
+})
+
+test('soma -10 + 3 é igual a -7', () => {
+    expect(math.add(-10, 3)).toBe(-7)
+})
+
+test('soma 10 + -30 é igual a -20', () => {
+    expect(math.add(10, -30)).toBe(-20)
+})
+
+// ---------------------------------------------------------------------
+
+test('subtratir 5 - 2 é igual a 3', () => {
+    expect(math.subtract(5, 2)).toBe(3)
+})
+
+test('subtratir -5 - 2 é igual a -7', () => {
+    expect(math.subtract(-5, 2)).toBe(-7)
+})
+
+// ---------------------------------------------------------------------
+
+test('multiplicar 5 * 5 é igual a 25', () => {
+    expect(math.multiply(5, 5)).toBe(25)
+})
+
+test('multiplicar 5 * -2 é igual a -10', () => {
+    expect(math.multiply(5, -2)).toBe(-10)
+})
+
+
+// ----------------------------------------------------------------------
+
+test('dividir 10 / 2 é igual a 5', () => {
+    expect(math.divide(10, 2)).toBe(5)
+})
+
+test('dividir 5 / 13 é igual a ?', () => {
+    expect(math.divide(5, 13)).toBe(0.38461538461538464)
 })
