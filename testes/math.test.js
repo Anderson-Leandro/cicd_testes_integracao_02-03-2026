b21acd9bef69623f8b873b9a88b2272784910117
Anderson-Leandro <106823753+Anderson-Leandro@users.noreply.github.com>
2026-03-03 00:15:47 +0000

    feature: Add new add test

--- testes/math.test.js
+++ testes/math.test.js
@@ -2,6 +2,10 @@ const math = require('../math');
 
 
 
-test('soma 2 + 3 igual a 5', () => { 
-expect(math.add(2, 3)).toBe(5); 
-}); 
+test('soma 2 + 3 igual a 5', () => {
+    expect(math.add(2, 3)).toBe(5);
+});
+
+test('soma 5 + 3 é igual a 8', () => {
+    expect(math.add(5, 3)).toBe(8);
+})
